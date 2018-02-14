d3b127b9dcef1b12c45a8b5b2c623966d89dbf5b
Rakesh Choudavarapu <Rakesh.Choudavarapu@jda.com>
2018-02-14 15:40:28 +0530

    made few changes for root handle

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var logger = require('morgan'),
   cors = require('cors'),
   http = require('http'),
   express = require('express'),
-  errorhandler = require('errorhandler'),
+  //errorhandler = require('errorhandler'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   apiai = require('apiai'),
@@ -19,10 +19,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(logger('dev'));
-  app.use(errorhandler())
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(logger('dev'));
+//   app.use(errorhandler())
+// }
 
 var port = process.env.PORT || 3001;
 
